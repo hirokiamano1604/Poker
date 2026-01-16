e3edf8d7906cd615496f64b94ae9eddb665e6f08
hirokiamano1604 <hirokiamano@outlook.com>
2026-01-15 17:50:26 -0800

    Module 2,3

---
+++ PythonForPortfolio/Module 1/edhec_risk_kit.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import scipy.stats
+import numpy as np
+
+def drawdown(return_series: pd.Series):
+    """
+    It takes a time-series of asset returns
+    Compute and returns a Dataframe that contains:
+        the wealth index
+        the previous peaks
+        percent drawdowns
+    """
+
+    wealth_index = 1000 * (1+ return_series).cumprod()
+    previous_peaks = wealth_index.cummax()
+    drawdowns = (wealth_index - previous_peaks) / previous_peaks
+    return pd.DataFrame({
+        "Wealth" : wealth_index,
+        "Peaks" : previous_peaks,
+        "Drawdown" : drawdowns
+    })
+
+def gett_ffme_returns():
+    """
+    Load the Fama-French Dataset for the returns of the Top and Bottom Deciles by MarketCap 
+    """
+    me_m = pd.read_csv('../data/Portfolios_Formed_on_ME_monthly_EW.csv', header = 0, index_col = 0,na_values = -99.99)
+    rets = me_m[['Lo 10', 'Hi 10']]
+    rets.columns = ['SmallCap', 'LargeCap']
+    rets = rets/100
+    rets.index = pd.to_datetime(rets.index, format = '%Y%m').to_period('M')
+    return rets
+
+def get_hfi_returns():
+    """
+    Load and format the EHEC Hedg Fund Index Returns
+    """
+    hfi = pd.read_csv("../data/edhec-hedgefundindices.csv",
+                      header = 0, index_col = 0, parse_dates=True)
+    hfi = hfi/100
+    hfi.index = hfi.index.to_period("M")
+    return hfi
+
+def semideviation(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a Series or a DataFrame
+    """
+    is_negative = r < 0
+    return r[is_negative].std(ddof=0)
+
+def semideviation3(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a Series or a DataFrame, else raises a TypeError
+    """
+    excess= r-r.mean()                                        # We demean the returns
+    excess_negative = excess[excess<0]                        # We take only the returns below the mean
+    excess_negative_square = excess_negative**2               # We square the demeaned returns below the mean
+    n_negative = (excess<0).sum()                             # number of returns under the mean
+    return (excess_negative_square.sum()/n_negative)**0.5     # semideviation
+
+def skewness(r):
+    """
+    Alternative to scipy.stats.skew()
+    Computes the skewness of the supplied Series or DataFrame
+    Returns float or Series
+    """
+    demeaned_r = r - r.mean()
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**3).mean()
+    return exp/sigma_r**3
+def kurtosis(r):
+    """
+    Alternative to scipy.stats.skew()
+    Computes the kurtosis of the supplied Series or DataFrame
+    Returns float or Series
+    """
+    demeaned_r = r - r.mean()
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**4).mean()
+    return exp/sigma_r**4
+
+def is_normal(r,level=0.01):
+    """
+    Applies the Jarque-Bera test to determine if a Series is normal or not
+    Test is applied at the 1% level by default
+    Returns True if the hypothesis of normality is accepted, False otherwise
+    """
+    statistic ,p_value = scipy.stats.jarque_bera(r)
+    return p_value > level
+
+def var_historic(r,level =5):
+    """
+    returns the historic Value at Risk at a specified level
+    i.e. returns the number such that "level" percent of the returns
+    fall nelow thet number, and the (100-level) percent are above.
+    """
+    if isinstance(r,pd.DataFrame):
+        return r.aggregate(var_historic, level=level)
+    elif isinstance(r,pd.Series):
+        return -np.percentile(r,level)
+    else:
+        raise TypeError("Expected r to be Series or DataFrame")
+    
+from scipy.stats import norm
+def var_gaussian(r,level = 5,modified = False):
+    """
+    Returns the Pramaetric Gauusian VaR of a Series or DataFrame
+    If "modified is True, then the modified VaR is returned, using the Cornish-Fisher modification
+    """
+    z = norm.ppf(level/100)
+    if modified:
+        s = skewness(r)
+        k = kurtosis(r)
+        z = (z + (z**2-1) *s/6 + (z**3 - 3*z)*(k-3)/24 - (2*z**3-5*z)*(s**2)/36)
+    return -(r.mean()+z*r.std(ddof=0))
+
+def cvar_historic(r,level=5):
+    """
+    Computes the consditional VaR of Series or DataFrame
+    """
+    if isinstance(r,pd.Series):
+        is_beyond = r <= -var_historic(r,level = level)
+        return -r[is_beyond].mean()
+    elif isinstance(r,pd.DataFrame):
+        return r.aggregate(cvar_historic, level = level)
+    else:
+        raise TypeError("Expected r to be a Series or DataFrame")
